0bab12de3b0f4b4f3309f20505d496f27ade9069
Thomas Haß <50476660+hastho@users.noreply.github.com>
2023-10-04 14:03:40 +0200

    Update dos_inc.h
    add 4 DOS devices for LPT1-4

--- include/dos_inc.h
+++ include/dos_inc.h
@@ -78,7 +78,7 @@ enum { RETURN_EXIT=0,RETURN_CTRLC=1,RETURN_ABORT=2,RETURN_TSR=3};
 
 #define DOS_FILES   255
 #define DOS_DRIVES  26
-#define DOS_DEVICES 10
+#define DOS_DEVICES 14
 
 // dos swappable area is 0x320 bytes beyond the sysvars table
 // device driver chain is inside sysvars
